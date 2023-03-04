52b2582abf49aa47da5d0a2b5161b5cbde8ffbc8
dltorrise <dltorrise@gmail.com>
2023-03-04 11:01:37 -0600

    formatting email and alert

--- Assets/script.js
+++ Assets/script.js
@@ -1,23 +1,24 @@
+//DOM Elements
 const submit = document.getElementById("form-submit")
 const nameInput = document.getElementById("name-input")
 const messageInput = document.getElementById("message-input")
-
+//Security key
 const securityKey = 'f7c01209-da41-4015-bbcb-d44d0a465341'
 
-const message = "Thank you. Your soul has been harvested"
 
+//event listener
 submit.addEventListener('submit', (e) => {
     e.preventDefault()
     console.log('Submitted')
-
+//email code
     Email.send({
         SecureToken : securityKey,
         To : 'dltorrise@gmail.com',
         From : "dltorrise@gmail.com",
-        Subject : "Message from" + nameInput.value,
-        Body : "Message from SMTJS SERVER: \n\n" + messageInput.value
+        Subject : "Message from " + nameInput.value,
+        Body : `<h1>Message from SMTJS SERVER: </h1>\n\n" + ${messageInput.value}`
     }).then(
-      message => alert(message)
+      message => alert("Thank you. Your soul has been harvested.")
     );
     
 
