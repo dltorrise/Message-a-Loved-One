5ec8807c9173aeda59fd39d3f576731b08b1eda7
dltorrise <dltorrise@gmail.com>
2023-03-04 11:10:53 -0600

    email formatting and whitespace

--- Assets/script.js
+++ Assets/script.js
@@ -10,13 +10,15 @@ const securityKey = 'f7c01209-da41-4015-bbcb-d44d0a465341'
 submit.addEventListener('submit', (e) => {
     e.preventDefault()
     console.log('Submitted')
+    nameInput.value = ''
+    messageInput.value = ''
 //email code
     Email.send({
         SecureToken : securityKey,
         To : 'dltorrise@gmail.com',
         From : "dltorrise@gmail.com",
         Subject : "Message from " + nameInput.value,
-        Body : `<h1>Message from SMTJS SERVER: </h1>\n\n" + ${messageInput.value}`
+        Body : `<h3>Message from SMTJS SERVER: </h3>\n\n <p>${messageInput.value}</p>`
     }).then(
       message => alert("Thank you. Your soul has been harvested.")
     );
